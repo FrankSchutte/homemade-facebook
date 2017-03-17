e729817726ead4e553a7d22801665c2a6f9fb9ba
marius linders <marius.linders@hotmail.com>
2017-03-17 20:02:46 +0100

    fixed css stuff

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-/**
- * Created by Frank on 17-3-2017.
- */
-
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join('./build')));
-
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+app.get("/static/*", (req, res) => {
+	res.sendFile(path.join(__dirname, "build", req.path));
 });
 
-app.listen(port, function() {
-    console.log('Server started and listening to::' + port);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build/index.html"));
+});
+
+app.listen(port, () => {
+    console.log("Server started and listening to::" + port);
 });
