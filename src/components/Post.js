a97a4e50107b1d2eafb5f701da32319ea3fe5d4d
Unknown <albertveldman96@gmail.com>
2017-03-17 16:10:45 +0100

    Like knop toegevoegd

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,13 +4,19 @@ import Comments from "./Comments";
 import {formatDateTime} from "../utils/date";
 import "./Post.css";
 import {getCookie} from "../utils/cookie";
+import {users} from "../store";
 
 class Post extends Component {
     constructor(props) {
         super(props);
+
+        let user = users[getCookie("loggedIn")];
+        let liked = user.liked_posts.keys()[this.props.data._id];
+
         this.state = {
             comments: this.props.data.comments,
-            likes: this.props.data.likes
+            likes: this.props.data.likes,
+            liked: liked
         };
         this.state.color = 'hsla(' + (Math.random() * 360) + ', 50%, 50%, .5)';
     }
@@ -27,7 +33,14 @@ class Post extends Component {
     };
 
     handleNewLike = like => {
-      console.log(like);
+        if (this.state.liked) {
+            this.state.likes--;
+            this.setState({liked: false});
+        }
+        else {
+            this.state.likes++;
+            this.setState({liked: true});
+        }
     };
 
     render() {
@@ -42,6 +55,7 @@ class Post extends Component {
                     <span className="author">By: {author}</span>
                     <span className="dateModified">On: {formatDateTime(dateModified)}</span>
                     <span className="likes">{likes} likes</span>
+                        <button onClick={this.handleNewLike}>{this.state.liked ? "Unlike" : "Like"}</button>
                 </div>
                 <h3>Comments:</h3>
                 <Comments handleNewComment={this.handleNewComment} data={comments}/>
